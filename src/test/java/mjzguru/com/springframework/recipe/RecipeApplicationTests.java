31991fb2c4e8cab6962d01c633884b6573987912
MajidJowDev <majid.jow@gmail.com>
2021-11-21 17:10:07 +0330

    7- Initiate Commit

---
+++ src/test/java/mjzguru/com/springframework/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package mjzguru.com.springframework.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
