0aee5d158f193f1f9d45872a22504de361df95ca
MajidJowDev <majid.jow@gmail.com>
2021-12-12 01:36:33 +0330

    10- 4. Creating a JUnit Test. added Junit 4 in dependencies and implemented a simple test for Category (CategoryTest)

--- src/test/java/mjzguru/com/springframework/recipe/RecipeApplicationTests.java
+++ src/test/java/mjzguru/com/springframework/recipe/RecipeApplicationTests.java
@@ -1,8 +1,11 @@
 package mjzguru.com.springframework.recipe;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class) // for JUnit 4 // brings Spring Context in Test
 @SpringBootTest
 class RecipeApplicationTests {
 
---
+++ src/test/java/mjzguru/com/springframework/recipe/domain/CategoryTest.java
@@ -0,0 +1,32 @@
+package mjzguru.com.springframework.recipe.domain;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before // added before testing methods in current unit test class, (to be used for testing some of or all methods)
+    public void setUp(){
+        category = new Category();
+    }
+
+    @org.junit.Test
+    public void getId() {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @org.junit.Test
+    public void getDescription() {
+    }
+
+    @org.junit.Test
+    public void getRecipes() {
+    }
+}
