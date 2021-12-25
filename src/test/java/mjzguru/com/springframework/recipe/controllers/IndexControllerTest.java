827172f8f4d127e78389837178b03a5371806c71
MajidJowDev <majid.jow@gmail.com>
2021-12-26 01:20:07 +0330

    10- 7. Assignment Review

--- src/test/java/mjzguru/com/springframework/recipe/controllers/IndexControllerTest.java
+++ src/test/java/mjzguru/com/springframework/recipe/controllers/IndexControllerTest.java
@@ -11,6 +11,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -40,12 +42,15 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
-        String returnValue = "index";
+        String viewNameReturnValue = "index";
 
-        assertEquals(indexController.getIndexPage(model), returnValue);
+        //assertEquals(indexController.getIndexPage(model), returnValue);
+        assertEquals(viewNameReturnValue,indexController.getIndexPage(model)); // first param = expected, second param = actual
 
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute("recipes", recipeService.getRecipes());
+        //verify(model, times(1)).addAttribute("recipes", recipeService.getRecipes());
+        // also checks if passed params to addAttribute method are like the following (first param is "recipe" amd second param is some Set or List
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
 
     }
 }
