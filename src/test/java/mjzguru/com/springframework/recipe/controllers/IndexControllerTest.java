10a906631feed4fcb3d1a25039516d8f24929f04
MajidJowDev <majid.jow@gmail.com>
2021-12-26 03:02:09 +0330

    10- 8. Mockito Argument Capture. used ArgumentCaptor to check the correctness of Arguments for the addAttribute method

--- src/test/java/mjzguru/com/springframework/recipe/controllers/IndexControllerTest.java
+++ src/test/java/mjzguru/com/springframework/recipe/controllers/IndexControllerTest.java
@@ -1,20 +1,24 @@
 package mjzguru.com.springframework.recipe.controllers;
 
+import mjzguru.com.springframework.recipe.domain.Recipe;
 import mjzguru.com.springframework.recipe.repositories.CategoryRepository;
 import mjzguru.com.springframework.recipe.repositories.RecipeRepository;
 import mjzguru.com.springframework.recipe.repositories.UnitOfMeasureRepository;
 import mjzguru.com.springframework.recipe.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
 
@@ -42,15 +46,34 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
-        String viewNameReturnValue = "index";
 
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        //recipes.add(new Recipe()); // removed because java take it as an equal object as the object in above line and merge them as one Object
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //When
+        String viewNameReturnValue = "index"; // expected value
+
+        //then
         //assertEquals(indexController.getIndexPage(model), returnValue);
         assertEquals(viewNameReturnValue,indexController.getIndexPage(model)); // first param = expected, second param = actual
 
         verify(recipeService, times(1)).getRecipes();
         //verify(model, times(1)).addAttribute("recipes", recipeService.getRecipes());
         // also checks if passed params to addAttribute method are like the following (first param is "recipe" amd second param is some Set or List
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        //verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+
 
     }
 }
