d085f4db6c998858bd0ddf892286ba4d703a5f1f
MajidJowDev <majid.jow@gmail.com>
2022-01-08 23:14:12 +0330

    11- 8. Create a Recipe. modified RecipeController to handle receving and saving New recipes from form, and added html file and configed actions for ThymeLeaf

--- src/main/java/mjzguru/com/springframework/recipe/controllers/RecipeController.java
+++ src/main/java/mjzguru/com/springframework/recipe/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package mjzguru.com.springframework.recipe.controllers;
 
+import mjzguru.com.springframework.recipe.commands.RecipeCommand;
 import mjzguru.com.springframework.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -16,11 +16,33 @@ public class RecipeController {
     }
 
     @RequestMapping("/recipe/show/{id}")
-    public String showById(@PathVariable String id, Model model){
+    public String showById(@PathVariable String id, Model model){ // @PathVariable mathces the variables in url with the method params
 
         //model.addAttribute("recipe", recipeService.findById(new Long(id)));
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    //adding the view capability
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    //adding the post back
+    //@RequestMapping(name = "recipe", method = RequestMethod.POST)
+    @PostMapping
+    @RequestMapping( "recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        // ModelAttribute tells spring to bind form post parameters to RecipeCommand object and
+        //it will happen automatically by the naming convention of props that we did in the form
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        //redirect: tells Spring MVC to redirect to a url
+        return "redirect:/recipe/show/" + savedCommand.getId();
+
     }
 }
