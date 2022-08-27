b723ea9233e224681d5bd5b1b97b879af97c0096
MajidJowDev <majid.jow@gmail.com>
2022-08-28 01:48:58 +0430

    22- 12. WebFlux Controller Advice ... modifyed ControllerExceptionHandler to support WebFlux

--- src/main/java/mjzguru/com/springframework/recipe/controllers/ControllerExceptionHandler.java
+++ src/main/java/mjzguru/com/springframework/recipe/controllers/ControllerExceptionHandler.java
@@ -1,7 +1,12 @@
 package mjzguru.com.springframework.recipe.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice //By default, the methods in an @ControllerAdvice apply globally to all controllers
@@ -9,22 +14,29 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 // so that it can be used globally between all controllers
 public class ControllerExceptionHandler {
 
-    /*
+
     // we moved this method here so that all controllers can use it globally
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormat(Exception exception) { // here we do not need to implement NumberFormat Exception for the Recipe
+    //@ExceptionHandler(NumberFormatException.class)  // use this in spring MVC
+    @ExceptionHandler(WebExchangeBindException.class) // use this in webflux
+    public String handleNumberFormat(Exception exception, Model model) { // here we do not need to implement NumberFormat Exception for the Recipe
         // service the exception param will be filled with default sys message
 
         log.error("Handling Number Format exception!");
         log.error(exception.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
+        model.addAttribute("exception", exception);
+
+        return "400error";
+
+        /*
+        ModelAndView modelAndView = new ModelAndView(); // modelAndView is used specifically for servlet API and does not work in Reactive environment
 
         modelAndView.setViewName("400error"); // for bad request we should use status code 400
         modelAndView.addObject("exception", exception); // added to display the exception info on the ErrorPage
 
         return modelAndView;
+        */
     }
-    */
+
 }
